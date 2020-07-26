f03e1444f606e0a788c40efd527d683307ccfa46
Scott Doyen <srdoyen@gmail.com>
2020-07-25 22:06:55 -0700

    began ignoring config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "2820422348193104",
-    clientSecret: "5c670712931e4af3141986aa93e6e18a",
-  },
-};
